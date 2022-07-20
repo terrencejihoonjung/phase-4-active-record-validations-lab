e4ec10ee7b229a58250ba5309031ffe9cef02b72
--replace-all <terrencejung928@gmail.com>
2022-07-20 15:38:26 -0700

    commit tree d47d892849177f1f62e6b05348f6d05b4c4cc035

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+
+    validate :is_clickbaity
+
+    PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def is_clickbaity
+        if PATTERNS.none? { |pattern| pattern.match(title) }
+            errors.add(:title, "Not a click-baity title")
+        end
+    end
 end
